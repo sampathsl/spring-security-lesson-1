2c1b91809ca4d1e44e95a1d0f9f3fca14b044f4a
Sampath Thennakoon <sampath.20@cse.mrt.ac.lk>
2023-09-30 11:52:31 +0530

    feat: add in memory users - lesson 24

--- src/main/java/com/sampatht/springsecurity/lesson/config/SecurityConfig.java
+++ src/main/java/com/sampatht/springsecurity/lesson/config/SecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,10 +16,33 @@ public class SecurityConfig {
   @Bean
   @Order(SecurityProperties.BASIC_AUTH_ORDER)
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-    return http.authorizeHttpRequests((requests) -> requests.requestMatchers("/account", "/balance", "/loan", "/card").authenticated()
-                    .requestMatchers("/notice", "/contact").permitAll())
-            .csrf(csrf -> csrf.disable())
-            .formLogin(Customizer.withDefaults())
-            .httpBasic(Customizer.withDefaults()).build();
+    return http.authorizeHttpRequests(
+            (requests) ->
+                requests
+                    .requestMatchers("/account", "/balance", "/loan", "/card")
+                    .authenticated()
+                    .requestMatchers("/notice", "/contact")
+                    .permitAll())
+        .csrf(csrf -> csrf.disable())
+        .formLogin(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults())
+        .build();
+  }
+
+  @Bean
+  public InMemoryUserDetailsManager inMemoryUserDetailsManagerService() {
+    UserDetails admin =
+        User.withDefaultPasswordEncoder()
+            .username("admin")
+            .password("add")
+            .authorities("ADMIN")
+            .build();
+    UserDetails clientOne =
+        User.withDefaultPasswordEncoder()
+            .username("client1")
+            .password("client1")
+            .authorities("READ")
+            .build();
+    return new InMemoryUserDetailsManager(admin, clientOne);
   }
 }
